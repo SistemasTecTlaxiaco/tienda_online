c40b37ae5f3a551f1354b44a220d25ba07231060
Joel343 <953143joel@gmail.com>
2021-05-26 02:05:52 -0500

    updated

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Pagina No Encontrada</title>
+	<title>Pagina no encontrada</title>
 </head>
 <body>
-	<h1>Pagina No Encontrada</h1>
+	<h1>Pagina no encontrada</h1>
 </body>
 </html>
