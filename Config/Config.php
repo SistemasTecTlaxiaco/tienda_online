2c297866f450066accc1133005b75ea6d8549f2a
JulioCesar69 <juliocrack912@gmail.com>
2021-06-17 14:31:22 -0500

    Configuración url base

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	//define("BASE_URL", "http://localhost/tienda_virtual/");
-	const BASE_URL = "http://localhost/tienda_online/";
+	const BASE_URL = "http://localhost/tienda_online";
 
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
