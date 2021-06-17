2d24f0f6eafc37966c2caa71f8b193a044bbbd27
JulioCesar69 <juliocrack912@gmail.com>
2021-06-17 14:21:40 -0500

    Reconfiguración url local

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	//define("BASE_URL", "http://localhost/tienda_virtual/");
-	const BASE_URL = "https://tiendavirtual69.herokuapp.com/";
+	const BASE_URL = "http://localhost/tienda_online/";
 
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
