4478e627ccea62c3098e442403c6402a6737ed98
Guadalupe Quiroz <guadalupe.quiroz.g@gmail.com>
2021-06-22 22:18:02 -0500

    Modificacion de parametros de conexión

--- Config/Config.php
+++ Config/Config.php
@@ -1,16 +1,16 @@
 <?php 
 	
 	//define("BASE_URL", "http://localhost/tienda_virtual/");
-	const BASE_URL = "http://localhost/tienda_online";
+	const BASE_URL = "https://tiendapcbox.herokuapp.com/";
 
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
 
 	//Datos de conexión a Base de Datos
-	const DB_HOST = "localhost";
-	const DB_NAME = "db_tiendavirtual";
-	const DB_USER = "root";
-	const DB_PASSWORD = "";
+	const DB_HOST = "dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	const DB_NAME = "xs74953pb0hvmidl";
+	const DB_USER = "y0k79cxzb1n7lj9r";
+	const DB_PASSWORD = "gk6clc7wdkfdevfw";
 	const DB_CHARSET = "utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
