30edbacc91cf953f4a7e5da317fcfb0523c60885
Joel343 <953143joel@gmail.com>
2021-09-21 14:06:35 -0500

    Diseñar Formulario Registro de Productos

--- Views/Template/Modals/modalProductos.php
+++ Views/Template/Modals/modalProductos.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="modalFormProductos" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" >
+  <div class="modal-dialog modal-xl" >
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nueva Producto</h5>
@@ -11,35 +11,97 @@
       <div class="modal-body">
             <form id="formProductos" name="formProductos" class="form-horizontal">
               <input type="hidden" id="idProducto" name="idProducto" value="">
-              
               <p class="text-primary">Los campos con asterisco (<span class="required">*</span>) son obligatorios.</p>
-              
               <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-8">
                     <div class="form-group">
-                      <label class="control-label">Nombre <span class="required">*</span></label>
-                      <input class="form-control" id="txtNombre" name="txtNombre" type="text" placeholder="Nombre Categoría" required="">
+                      <label class="control-label">Nombre Producto <span class="required">*</span></label>
+                      <input class="form-control" id="txtNombre" name="txtNombre" type="text" required="">
                     </div>
                     <div class="form-group">
-                      <label class="control-label">Descripción <span class="required">*</span></label>
-                      <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" rows="2" placeholder="Descripción Categoría" required=""></textarea>
+                      <label class="control-label">Descripción Producto</label>
+                      <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" ></textarea>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleSelect1">Estado <span class="required">*</span></label>
-                        <select class="form-control selectpicker" id="listStatus" name="listStatus" required="">
-                          <option value="1">Activo</option>
-                          <option value="2">Inactivo</option>
-                        </select>
-                    </div>  
+                    
                 </div>
-                <div class="col-md-6">
-                   
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="control-label">Código <span class="required">*</span></label>
+                        <input class="form-control" id="txtCodigo" name="txtCodigo" type="text" placeholder="Código de barra" required="">
+                        <br>
+                        <div id="divBarCode" class="notblock textcenter">
+                            <div id="printCode">
+                                <svg id="barcode"></svg> 
+                            </div>
+                            <button class="btn btn-success btn-sm" type="button" onClick="fntPrintBarcode('#printCode')"><i class="fas fa-print"></i> Imprimir</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="control-label">Precio <span class="required">*</span></label>
+                            <input class="form-control" id="txtPrecio" name="txtPrecio" type="text" required="">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="control-label">Stock <span class="required">*</span></label>
+                            <input class="form-control" id="txtStock" name="txtStock" type="text" required="">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="listCategoria">Categoría <span class="required">*</span></label>
+                            <select class="form-control" data-live-search="true" id="listCategoria" name="listCategoria" required=""></select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="listStatus">Estado <span class="required">*</span></label>
+                            <select class="form-control selectpicker" id="listStatus" name="listStatus" required="">
+                              <option value="1">Activo</option>
+                              <option value="2">Inactivo</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                       <div class="form-group col-md-6">
+                           <button id="btnActionForm" class="btn btn-primary btn-lg btn-block" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>
+                       </div> 
+                       <div class="form-group col-md-6">
+                           <button class="btn btn-danger btn-lg btn-block" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
+                       </div> 
+                    </div>  
                 </div>
               </div>
               
               <div class="tile-footer">
-                <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
-                <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
+                 <div class="form-group col-md-12">
+                     <div id="containerGallery">
+                         <span>Agregar foto (440 x 545)</span>
+                         <button class="btnAddImage btn btn-info btn-sm" type="button">
+                             <i class="fas fa-plus"></i>
+                         </button>
+                     </div>
+                     <hr>
+                     <div id="containerImages">
+                         <!-- <div id="div24">
+                             <div class="prevImage">
+                                 <img src="<?= media(); ?>/images/uploads/producto1.jpg">
+                             </div>
+                             <input type="file" name="foto" id="img1" class="inputUploadfile">
+                             <label for="img1" class="btnUploadfile"><i class="fas fa-upload "></i></label>
+                             <button class="btnDeleteImage" type="button" onclick="fntDelItem('div24')"><i class="fas fa-trash-alt"></i></button>
+                         </div>
+                         <div id="div24">
+                             <div class="prevImage">
+                                 <img class="loading" src="<?= media(); ?>/images/loading.svg">
+                             </div>
+                             <input type="file" name="foto" id="img1" class="inputUploadfile">
+                             <label for="img1" class="btnUploadfile"><i class="fas fa-upload "></i></label>
+                             <button class="btnDeleteImage" type="button" onclick="fntDelItem('div24')"><i class="fas fa-trash-alt"></i></button>
+                         </div> -->
+                        
+                     </div>
+                 </div>
+                                
               </div>
             </form>
       </div>
@@ -48,11 +110,11 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="modalViewCategoria" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" >
+<div class="modal fade" id="modalViewProducto" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-xl" >
     <div class="modal-content">
       <div class="modal-header header-primary">
-        <h5 class="modal-title" id="titleModal">Datos de la categoría</h5>
+        <h5 class="modal-title" id="titleModal">Datos del Producto</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -61,24 +123,37 @@
         <table class="table table-bordered">
           <tbody>
             <tr>
-              <td>ID:</td>
-              <td id="celId"></td>
+              <td>Codigo:</td>
+              <td id="celCodigo"></td>
             </tr>
             <tr>
               <td>Nombres:</td>
               <td id="celNombre"></td>
             </tr>
             <tr>
+              <td>Precio:</td>
+              <td id="celPrecio"></td>
+            </tr>
+            <tr>
+              <td>Stock:</td>
+              <td id="celStock"></td>
+            </tr>
+            <tr>
+              <td>Categoría:</td>
+              <td id="celCategoria"></td>
+            </tr>
+            <tr>
+              <td>Status:</td>
+              <td id="celStatus"></td>
+            </tr>
+            <tr>
               <td>Descripción:</td>
               <td id="celDescripcion"></td>
             </tr>
             <tr>
-              <td>Estado:</td>
-              <td id="celEstado"></td>
-            </tr>
-            <tr>
-              <td>Foto:</td>
-              <td id="imgCategoria"></td>
+              <td>Fotos de referencia:</td>
+              <td id="celFotos">
+              </td>
             </tr>
           </tbody>
         </table>
