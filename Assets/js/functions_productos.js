d59b45657d420dab0fe4a4df2e0ac31f5ba6308d
Joel343 <953143joel@gmail.com>
2021-09-21 20:00:08 -0500

    Exportar y Personalizar columnas en DataTables

--- Assets/js/functions_productos.js
+++ Assets/js/functions_productos.js
@@ -27,28 +27,46 @@ window.addEventListener('load', function(){
             {"data":"status"},
             {"data":"options"}
         ],
+        "columnDefs": [
+            { 'className': "textcenter", "targets": [ 3 ] },
+            { 'className': "textright", "targets": [ 4 ] },
+            { 'className': "textcenter", "targets": [ 5 ] }
+          ],   
+
         'dom': 'lBfrtip',
         'buttons': [
             {
                 "extend": "copyHtml5",
                 "text": "<i class='far fa-copy'></i> Copiar",
                 "titleAttr":"Copiar",
-                "className": "btn btn-secondary"
+                "className": "btn btn-secondary",
+                "exportOptions": { 
+                    "columns": [ 0, 1, 2, 3, 4, 5] 
+                }
             },{
                 "extend": "excelHtml5",
                 "text": "<i class='fas fa-file-excel'></i> Excel",
                 "titleAttr":"Esportar a Excel",
-                "className": "btn btn-success"
+                "className": "btn btn-success",
+                "exportOptions": { 
+                    "columns": [ 0, 1, 2, 3, 4, 5] 
+                }
             },{
                 "extend": "pdfHtml5",
                 "text": "<i class='fas fa-file-pdf'></i> PDF",
                 "titleAttr":"Esportar a PDF",
-                "className": "btn btn-danger"
+                "className": "btn btn-danger",
+                "exportOptions": { 
+                    "columns": [ 0, 1, 2, 3, 4, 5] 
+                }
             },{
                 "extend": "csvHtml5",
                 "text": "<i class='fas fa-file-csv'></i> CSV",
                 "titleAttr":"Esportar a CSV",
-                "className": "btn btn-info"
+                "className": "btn btn-info",
+                "exportOptions": { 
+                    "columns": [ 0, 1, 2, 3, 4, 5] 
+                }
             }
         ],
         "resonsieve":"true",
