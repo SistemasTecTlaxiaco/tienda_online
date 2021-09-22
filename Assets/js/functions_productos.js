f9d8dfa706c938735c145c7a0e6f6105082d3a98
Joel343 <953143joel@gmail.com>
2021-09-21 19:03:56 -0500

    Listar y Paginar Productos de MySQL

--- Assets/js/functions_productos.js
+++ Assets/js/functions_productos.js
@@ -7,6 +7,55 @@ $(document).on('focusin', function(e) {
 });
 
 window.addEventListener('load', function(){
+
+    tableCategorias = $('#tableProductos').dataTable( {
+        "aProcessing":true,
+        "aServerSide":true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/Productos/getProductos",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"idproducto"},
+            {"data":"codigo"},
+            {"data":"nombre"},
+            {"data":"stock"},
+            {"data":"precio"},
+            {"data":"status"},
+            {"data":"options"}
+        ],
+        'dom': 'lBfrtip',
+        'buttons': [
+            {
+                "extend": "copyHtml5",
+                "text": "<i class='far fa-copy'></i> Copiar",
+                "titleAttr":"Copiar",
+                "className": "btn btn-secondary"
+            },{
+                "extend": "excelHtml5",
+                "text": "<i class='fas fa-file-excel'></i> Excel",
+                "titleAttr":"Esportar a Excel",
+                "className": "btn btn-success"
+            },{
+                "extend": "pdfHtml5",
+                "text": "<i class='fas fa-file-pdf'></i> PDF",
+                "titleAttr":"Esportar a PDF",
+                "className": "btn btn-danger"
+            },{
+                "extend": "csvHtml5",
+                "text": "<i class='fas fa-file-csv'></i> CSV",
+                "titleAttr":"Esportar a CSV",
+                "className": "btn btn-info"
+            }
+        ],
+        "resonsieve":"true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order":[[0,"desc"]]  
+    });
     fntCategorias();   
 }, false);
 
---
+++ Models/ProductosModel.php
@@ -0,0 +1,37 @@
+<?php 
+
+	class ProductosModel extends Mysql
+	{
+        private $intIdProducto;
+		private $strNombre;
+		private $strDescripcion;
+		private $intCodigo;
+		private $intCategoriaId;
+		private $intPrecio;
+		private $intStock;
+		private $intStatus;
+
+		public function __construct()
+		{
+			parent::__construct();
+		}	
+
+        public function selectProductos(){
+			$sql = "SELECT p.idproducto,
+							p.codigo,
+							p.nombre,
+							p.descripcion,
+							p.categoriaid,
+							c.nombre as categoria,
+							p.precio,
+							p.stock,
+							p.status 
+					FROM producto p 
+					INNER JOIN categoria c
+					ON p.categoriaid = c.idcategoria
+					WHERE p.status != 0 ";
+					$request = $this->select_all($sql);
+			return $request;
+		}	
+	}
+ ?>
