db9f886ce55ad66658908c461ab811d720b10dde
Joel343 <953143joel@gmail.com>
2021-09-23 18:23:32 -0500

    Configurar formulario actualizar productos

--- Assets/js/functions_productos.js
+++ Assets/js/functions_productos.js
@@ -1,81 +1,78 @@
 document.write(`<script src="${base_url}/Assets/js/plugins/JsBarcode.all.min.js"></script>`);
-
+let tableProductos;
+let rowTable = "";
 $(document).on('focusin', function(e) {
     if ($(e.target).closest(".tox-dialog").length) {
         e.stopImmediatePropagation();
     }
 });
-
-window.addEventListener('load', function(){
-
-    tableProductos = $('#tableProductos').dataTable( {
-        "aProcessing":true,
-        "aServerSide":true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
-        },
-        "ajax":{
-            "url": " "+base_url+"/Productos/getProductos",
-            "dataSrc":""
-        },
-        "columns":[
-            {"data":"idproducto"},
-            {"data":"codigo"},
-            {"data":"nombre"},
-            {"data":"stock"},
-            {"data":"precio"},
-            {"data":"status"},
-            {"data":"options"}
-        ],
-        "columnDefs": [
-            { 'className': "textcenter", "targets": [ 3 ] },
-            { 'className': "textright", "targets": [ 4 ] },
-            { 'className': "textcenter", "targets": [ 5 ] }
-          ],   
-
-        'dom': 'lBfrtip',
-        'buttons': [
-            {
-                "extend": "copyHtml5",
-                "text": "<i class='far fa-copy'></i> Copiar",
-                "titleAttr":"Copiar",
-                "className": "btn btn-secondary",
-                "exportOptions": { 
-                    "columns": [ 0, 1, 2, 3, 4, 5] 
-                }
-            },{
-                "extend": "excelHtml5",
-                "text": "<i class='fas fa-file-excel'></i> Excel",
-                "titleAttr":"Esportar a Excel",
-                "className": "btn btn-success",
-                "exportOptions": { 
-                    "columns": [ 0, 1, 2, 3, 4, 5] 
-                }
-            },{
-                "extend": "pdfHtml5",
-                "text": "<i class='fas fa-file-pdf'></i> PDF",
-                "titleAttr":"Esportar a PDF",
-                "className": "btn btn-danger",
-                "exportOptions": { 
-                    "columns": [ 0, 1, 2, 3, 4, 5] 
-                }
-            },{
-                "extend": "csvHtml5",
-                "text": "<i class='fas fa-file-csv'></i> CSV",
-                "titleAttr":"Esportar a CSV",
-                "className": "btn btn-info",
-                "exportOptions": { 
-                    "columns": [ 0, 1, 2, 3, 4, 5] 
-                }
+tableProductos = $('#tableProductos').dataTable( {
+    "aProcessing":true,
+    "aServerSide":true,
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+    },
+    "ajax":{
+        "url": " "+base_url+"/Productos/getProductos",
+        "dataSrc":""
+    },
+    "columns":[
+        {"data":"idproducto"},
+        {"data":"codigo"},
+        {"data":"nombre"},
+        {"data":"stock"},
+        {"data":"precio"},
+        {"data":"status"},
+        {"data":"options"}
+    ],
+    "columnDefs": [
+                    { 'className': "textcenter", "targets": [ 3 ] },
+                    { 'className': "textright", "targets": [ 4 ] },
+                    { 'className': "textcenter", "targets": [ 5 ] }
+                  ],       
+    'dom': 'lBfrtip',
+    'buttons': [
+        {
+            "extend": "copyHtml5",
+            "text": "<i class='far fa-copy'></i> Copiar",
+            "titleAttr":"Copiar",
+            "className": "btn btn-secondary",
+            "exportOptions": { 
+                "columns": [ 0, 1, 2, 3, 4, 5] 
             }
-        ],
-        "resonsieve":"true",
-        "bDestroy": true,
-        "iDisplayLength": 10,
-        "order":[[0,"desc"]]  
-    });
-
-    if(this.document.querySelector("#formProductos")){
+        },{
+            "extend": "excelHtml5",
+            "text": "<i class='fas fa-file-excel'></i> Excel",
+            "titleAttr":"Esportar a Excel",
+            "className": "btn btn-success",
+            "exportOptions": { 
+                "columns": [ 0, 1, 2, 3, 4, 5] 
+            }
+        },{
+            "extend": "pdfHtml5",
+            "text": "<i class='fas fa-file-pdf'></i> PDF",
+            "titleAttr":"Esportar a PDF",
+            "className": "btn btn-danger",
+            "exportOptions": { 
+                "columns": [ 0, 1, 2, 3, 4, 5] 
+            }
+        },{
+            "extend": "csvHtml5",
+            "text": "<i class='fas fa-file-csv'></i> CSV",
+            "titleAttr":"Esportar a CSV",
+            "className": "btn btn-info",
+            "exportOptions": { 
+                "columns": [ 0, 1, 2, 3, 4, 5] 
+            }
+        }
+    ],
+    "resonsieve":"true",
+    "bDestroy": true,
+    "iDisplayLength": 10,
+    "order":[[0,"desc"]]  
+});
+window.addEventListener('load', function() {
+    if(document.querySelector("#formProductos")){
         let formProductos = document.querySelector("#formProductos");
         formProductos.onsubmit = function(e) {
             e.preventDefault();
@@ -96,8 +93,8 @@ window.addEventListener('load', function(){
             divLoading.style.display = "flex";
             tinyMCE.triggerSave();
             let request = (window.XMLHttpRequest) ? 
-                new XMLHttpRequest() : 
-                new ActiveXObject('Microsoft.XMLHTTP');
+                            new XMLHttpRequest() : 
+                            new ActiveXObject('Microsoft.XMLHTTP');
             let ajaxUrl = base_url+'/Productos/setProducto'; 
             let formData = new FormData(formProductos);
             request.open("POST",ajaxUrl,true);
@@ -109,34 +106,50 @@ window.addEventListener('load', function(){
                     {
                         swal("", objData.msg ,"success");
                         document.querySelector("#idProducto").value = objData.idproducto;
-                        tableProductos.api().ajax.reload();
+                        document.querySelector("#containerGallery").classList.remove("notblock");
+
+                        if(rowTable == ""){
+                            tableProductos.api().ajax.reload();
+                        }else{
+                           htmlStatus = intStatus == 1 ? 
+                            '<span class="badge badge-success">Activo</span>' : 
+                            '<span class="badge badge-danger">Inactivo</span>';
+                            rowTable.cells[1].textContent = intCodigo;
+                            rowTable.cells[2].textContent = strNombre;
+                            rowTable.cells[3].textContent = intStock;
+                            rowTable.cells[4].textContent = smony+strPrecio;
+                            rowTable.cells[5].innerHTML =  htmlStatus;
+                            rowTable = ""; 
+                        }
                     }else{
                         swal("Error", objData.msg , "error");
                     }
                 }
-                    divLoading.style.display = "none";
-                    return false;
+                divLoading.style.display = "none";
+                return false;
             }
         }
     }
+
     if(document.querySelector(".btnAddImage")){
-        let btnAddImage =  document.querySelector(".btnAddImage");
-        btnAddImage.onclick = function(e){
-         let key = Date.now();
+       let btnAddImage =  document.querySelector(".btnAddImage");
+       btnAddImage.onclick = function(e){
+        let key = Date.now();
         let newElement = document.createElement("div");
         newElement.id= "div"+key;
-         newElement.innerHTML = ` 
-             <div class="prevImage"></div>
-             <input type="file" name="foto" id="img${key}" class="inputUploadfile">
-             <label for="img${key}" class="btnUploadfile"><i class="fas fa-upload "></i></label>
-             <button class="btnDeleteImage notblock" type="button" onclick="fntDelItem('#div${key}')"><i class="fas fa-trash-alt"></i></button>`;
-         document.querySelector("#containerImages").appendChild(newElement);
-         document.querySelector("#div"+key+" .btnUploadfile").click();
-         fntInputFile();
-        }
+        newElement.innerHTML = `
+            <div class="prevImage"></div>
+            <input type="file" name="foto" id="img${key}" class="inputUploadfile">
+            <label for="img${key}" class="btnUploadfile"><i class="fas fa-upload "></i></label>
+            <button class="btnDeleteImage notblock" type="button" onclick="fntDelItem('#div${key}')"><i class="fas fa-trash-alt"></i></button>`;
+        document.querySelector("#containerImages").appendChild(newElement);
+        document.querySelector("#div"+key+" .btnUploadfile").click();
+        fntInputFile();
+       }
     }
+
     fntInputFile();
-    fntCategorias();   
+    fntCategorias();
 }, false);
 
 if(document.querySelector("#txtCodigo")){
@@ -152,8 +165,8 @@ if(document.querySelector("#txtCodigo")){
 }
 
 tinymce.init({
-    selector: '#txtDescripcion',
-    width: "100%",
+	selector: '#txtDescripcion',
+	width: "100%",
     height: 400,    
     statubar: true,
     plugins: [
@@ -215,6 +228,32 @@ function fntInputFile(){
     });
 }
 
+function fntDelItem(element){
+    let nameImg = document.querySelector(element+' .btnDeleteImage').getAttribute("imgname");
+    let idProducto = document.querySelector("#idProducto").value;
+    let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    let ajaxUrl = base_url+'/Productos/delFile'; 
+
+    let formData = new FormData();
+    formData.append('idproducto',idProducto);
+    formData.append("file",nameImg);
+    request.open("POST",ajaxUrl,true);
+    request.send(formData);
+    request.onreadystatechange = function(){
+        if(request.readyState != 4) return;
+        if(request.status == 200){
+            let objData = JSON.parse(request.responseText);
+            if(objData.status)
+            {
+                let itemRemove = document.querySelector(element);
+                itemRemove.parentNode.removeChild(itemRemove);
+            }else{
+                swal("", objData.msg , "error");
+            }
+        }
+    }
+}
+
 function fntViewInfo(idProducto){
     let request = (window.XMLHttpRequest) ? 
                     new XMLHttpRequest() : 
@@ -232,6 +271,7 @@ function fntViewInfo(idProducto){
                 let estadoProducto = objProducto.status == 1 ? 
                 '<span class="badge badge-success">Activo</span>' : 
                 '<span class="badge badge-danger">Inactivo</span>';
+
                 document.querySelector("#celCodigo").innerHTML = objProducto.codigo;
                 document.querySelector("#celNombre").innerHTML = objProducto.nombre;
                 document.querySelector("#celPrecio").innerHTML = objProducto.precio;
@@ -248,13 +288,109 @@ function fntViewInfo(idProducto){
                 }
                 document.querySelector("#celFotos").innerHTML = htmlImage;
                 $('#modalViewProducto').modal('show');
+
+            }else{
+                swal("Error", objData.msg , "error");
+            }
+        }
+    } 
+}
+
+function fntEditInfo(element,idProducto){
+    rowTable = element.parentNode.parentNode.parentNode;
+    document.querySelector('#titleModal').innerHTML ="Actualizar Producto";
+    document.querySelector('.modal-header').classList.replace("headerRegister", "headerUpdate");
+    document.querySelector('#btnActionForm').classList.replace("btn-primary", "btn-info");
+    document.querySelector('#btnText').innerHTML ="Actualizar";
+    let request = (window.XMLHttpRequest) ? 
+                    new XMLHttpRequest() : 
+                    new ActiveXObject('Microsoft.XMLHTTP');
+    let ajaxUrl = base_url+'/Productos/getProducto/'+idProducto;
+    request.open("GET",ajaxUrl,true);
+    request.send();
+    request.onreadystatechange = function(){
+        if(request.readyState == 4 && request.status == 200){
+            let objData = JSON.parse(request.responseText);
+            if(objData.status)
+            {     
+                let htmlImage = "";           
+                let objProducto = objData.data;
+                console.log(objProducto);
+                
+                document.querySelector("#idProducto").value = objProducto.idproducto;
+                document.querySelector("#txtNombre").value = objProducto.nombre;
+                document.querySelector("#txtDescripcion").value = objProducto.descripcion;
+                document.querySelector("#txtCodigo").value = objProducto.codigo;
+                document.querySelector("#txtPrecio").value = objProducto.precio;
+                document.querySelector("#txtStock").value = objProducto.stock;
+                document.querySelector("#listCategoria").value = objProducto.categoriaid;
+                document.querySelector("#listStatus").value = objProducto.status;
+                tinymce.activeEditor.setContent(objProducto.descripcion); 
+                $('#listCategoria').selectpicker('render');
+                $('#listStatus').selectpicker('render');
+                fntBarcode(); 
+                
+                if(objProducto.images.length > 0){
+                    let objProductos = objProducto.images;
+                    for (let p = 0; p < objProductos.length; p++) {
+                        let key = Date.now()+p;
+                        htmlImage +=`<div id="div${key}">
+                            <div class="prevImage">
+                            <img src="${objProductos[p].url_image}"></img>
+                            </div>
+                            <button type="button" class="btnDeleteImage" onclick="fntDelItem('#div${key}')" imgname="${objProductos[p].img}">
+                            <i class="fas fa-trash-alt"></i></button></div>`;
+                    }
+                }
+                document.querySelector("#containerImages").innerHTML = htmlImage; 
+                document.querySelector("#divBarCode").classList.remove("notblock");
+                $('#modalFormProductos').modal('show');
             }else{
                 swal("Error", objData.msg , "error");
             }
         }
     }
-    $('#modalViewProducto').modal('show');
 }
+
+function fntDelInfo(idProducto){
+    swal({
+        title: "Eliminar Producto",
+        text: "¿Realmente quiere eliminar el producto?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si, eliminar!",
+        cancelButtonText: "No, cancelar!",
+        closeOnConfirm: false,
+        closeOnCancel: true
+    }, function(isConfirm) {
+        
+        if (isConfirm) 
+        {
+            let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            let ajaxUrl = base_url+'/Productos/delProducto';
+            let strData = "idProducto="+idProducto;
+            request.open("POST",ajaxUrl,true);
+            request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            request.send(strData);
+            request.onreadystatechange = function(){
+                if(request.readyState == 4 && request.status == 200){
+                    let objData = JSON.parse(request.responseText);
+                    if(objData.status)
+                    {
+                        swal("Eliminar!", objData.msg , "success");
+                        tableProductos.api().ajax.reload();
+                    }else{
+                        swal("Atención!", objData.msg , "error");
+                    }
+                }
+            }
+        }
+
+    });
+
+}
+
+
 
 function fntCategorias(){
     if(document.querySelector('#listCategoria')){
@@ -272,6 +408,7 @@ function fntCategorias(){
         }
     }
 }
+
 function fntBarcode(){
     let codigo = document.querySelector("#txtCodigo").value;
     JsBarcode("#barcode", codigo);
@@ -293,8 +430,11 @@ function openModal()
     document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
     document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
     document.querySelector('#btnText').innerHTML ="Guardar";
-    document.querySelector('#titleModal').innerHTML = "Nueva Producto";
+    document.querySelector('#titleModal').innerHTML = "Nuevo Producto";
     document.querySelector("#formProductos").reset();
+    document.querySelector("#divBarCode").classList.add("notblock");
+    document.querySelector("#containerGallery").classList.add("notblock");
+    document.querySelector("#containerImages").innerHTML = "";
     $('#modalFormProductos').modal('show');
-  
+
 }
