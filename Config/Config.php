08ff1d2846f36d97d88d16d8ef3cce5a00a9d8ea
MaGuadalupeQuiroz <72761157+MaGuadalupeQuiroz@users.noreply.github.com>
2021-09-27 21:39:50 -0500

    conflicto corrección

--- Config/Config.php
+++ Config/Config.php
@@ -6,19 +6,19 @@
 	date_default_timezone_set('America/Mexico_City');
 
 	//Datos de conexión a Base de Datos
-	define ('DB_HOST', 'dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-	define ('DB_NAME', 'xs74953pb0hvmidl');
-	define ('DB_USER', 'y0k79cxzb1n7lj9r');
-	define ('DB_PASSWORD', 'gk6clc7wdkfdevfw');
-	define ('DB_CHARSET', 'utf8');
+	const DB_HOST = "";
+	const DB_NAME = "db_tiendavirtual";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
 	//define("BASE_URL", "http://localhost/tienda_virtual/");
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
-	define ('SPD',".");
-	define ('SPM',",");
+	const SPD = ".";
+	const SPM = ",";
 
 	//Simbolo de moneda
-	define ('SMONEY',"$");
+	const 'SMONEY',"$";
 
 	
 
