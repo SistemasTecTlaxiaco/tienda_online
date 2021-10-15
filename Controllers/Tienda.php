67529f6e9cb8c41336ce038625352611fdfdc37c
Joel343 <953143joel@gmail.com>
2021-10-15 13:41:21 -0500

    Guardar venta en la base de datos - Parte 1

--- Controllers/Tienda.php
+++ Controllers/Tienda.php
@@ -238,8 +238,22 @@
 			die();
 		}
         public function procesarVenta(){
-			dep($_POST);
-			die();
+			if($_POST){
+				$idtransaccionpaypal = NULL;
+				$datospaypal = NULL;
+				$personaid = $_SESSION['idUser'];
+				$monto = 0;
+				$tipopagoid = intval($_POST['inttipopago']);
+				$direccionenvio = strClean($_POST['direccion']).', '.strClean($_POST['ciudad']);
+				$status = "Pendiente";
+				$subtotal = 0;
+
+			}else{
+				$arrResponse = array("status" => false, "msg" => 'No es posible procesar el pedido.');
+			}
+			echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+            die();
 		}
 	}
  ?>
+ 
