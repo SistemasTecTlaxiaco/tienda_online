517e3afacd36ca7f3e4b33784fc149ab731bf364
Guadalupe Quiroz <guadalupe.quiroz.g@gmail.com>
2021-12-14 23:51:28 -0600

    Config

--- Config/Config.php
+++ Config/Config.php
@@ -6,11 +6,19 @@
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
 
-	//Datos de conexión a Base de Datos
+	/*Datos de conexión a Base de Datos
 	define ('DB_HOST', 'dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 	define ('DB_NAME', 'xs74953pb0hvmidl');
 	define ('DB_USER', 'y0k79cxzb1n7lj9r');
 	define ('DB_PASSWORD', 'gk6clc7wdkfdevfw');
+	define ('DB_CHARSET', 'utf8');
+	//define("BASE_URL", "http://localhost/tienda_virtual/")
+*/
+	//Datos de conexión a Base de Datos
+	define ('DB_HOST', 'localhost:3307');
+	define ('DB_NAME', 'db_tiendavirtual');
+	define ('DB_USER', 'root');
+	define ('DB_PASSWORD', '');
 	define ('DB_CHARSET', 'utf8');
 	//define("BASE_URL", "http://localhost/tienda_virtual/")
 
