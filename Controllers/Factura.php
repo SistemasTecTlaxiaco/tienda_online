e15e5ec62d70ddc75079d3ff4d05714b301979e5
Guadalupe Quiroz <guadalupe.quiroz.g@gmail.com>
2021-12-15 16:17:21 -0600

    Config Vista de PDF a clientes

--- Controllers/Factura.php
+++ Controllers/Factura.php
@@ -13,13 +13,20 @@
 		{
 			if(is_numeric($idpedido)){
 				$idpersona = "";
+				if($_SESSION['permisosMod']['r'] and $_SESSION['userData']['idrol'] == RCLIENTES){
+					$idpersona = $_SESSION['userData']['idpersona'];
+				}
 				$data = $this->model->selectPedido($idpedido,$idpersona);
-				
+				if(empty($data)){
+					echo "Datos no encontrados";
+				}else{
+
 					ob_end_clean();
 					$html = getFile("Template/Modals/comprobantePDF",$data);
-					$html2pdf = new Html2Pdf();
+					$html2pdf = new Html2Pdf('p','A4','es','true','UTF-8');
 					$html2pdf->writeHTML($html);
-					$html2pdf->output('factura-'.$idpedido.'.pdf');				
+					$html2pdf->output('factura-'.$idpedido.'.pdf');
+				}
 			}else{
 				echo "Dato no válido";
 			}
