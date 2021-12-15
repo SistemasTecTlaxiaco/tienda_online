ea65630190ecaa6f66b2bde0be25a0bedd1ba19f
Guadalupe Quiroz <guadalupe.quiroz.g@gmail.com>
2021-12-15 16:39:49 -0600

    Config acceso a PDF

--- Controllers/Factura.php
+++ Controllers/Factura.php
@@ -7,28 +7,40 @@
 		{
 			parent::__construct();
 			session_start();
+			session_regenerate_id(true);
+			if(empty($_SESSION['login']))
+			{
+				header('Location: '.base_url().'/login');
+				die();
+			}
+			getPermisos(MPEDIDOS);
 		}
 
 		public function generarFactura($idpedido)
 		{
-			if(is_numeric($idpedido)){
-				$idpersona = "";
-				if($_SESSION['permisosMod']['r'] and $_SESSION['userData']['idrol'] == RCLIENTES){
-					$idpersona = $_SESSION['userData']['idpersona'];
-				}
-				$data = $this->model->selectPedido($idpedido,$idpersona);
-				if(empty($data)){
-					echo "Datos no encontrados";
+			if($_SESSION['permisosMod']['r']){
+				if(is_numeric($idpedido)){
+					$idpersona = "";
+					if($_SESSION['permisosMod']['r'] and $_SESSION['userData']['idrol'] == RCLIENTES){
+						$idpersona = $_SESSION['userData']['idpersona'];
+					}
+					$data = $this->model->selectPedido($idpedido,$idpersona);
+					if(empty($data)){
+						echo "Datos no encontrados";
+					}else{
+						$idpedido = $data['orden']['idpedido'];
+						ob_end_clean();
+						$html = getFile("Template/Modals/comprobantePDF",$data);
+						$html2pdf = new Html2Pdf('p','A4','es','true','UTF-8');
+						$html2pdf->writeHTML($html);
+						$html2pdf->output('factura-'.$idpedido.'.pdf');
+					}
 				}else{
-					$idpedido = $data['orden']['idpedido'];
-					ob_end_clean();
-					$html = getFile("Template/Modals/comprobantePDF",$data);
-					$html2pdf = new Html2Pdf('p','A4','es','true','UTF-8');
-					$html2pdf->writeHTML($html);
-					$html2pdf->output('Factura-'.$idpedido.'.pdf');
+					echo "Dato no válido";
 				}
 			}else{
-				echo "Dato no válido";
+				header('Location: '.base_url().'/login');
+				die();
 			}
 		}
 
