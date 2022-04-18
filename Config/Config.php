f1ef236b25f1f41f321663b468d70c2aa9788808
JulioCesar69 <juliocrack912@gmail.com>
2022-04-18 15:59:30 -0500

    Configuracion rutas local

--- Config/Config.php
+++ Config/Config.php
@@ -1,29 +1,29 @@
 <?php 
 	
 	//define('BASE_URL', 'http://localhost/tienda_virtual/');
-	define ('BASE_URL', 'https://tiendapcbox.herokuapp.com');
+	define ('BASE_URL', 'https://localhost/tienda_online/	');
 	
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
 
 	//Datos de conexión a Base de Datos
-	define ('DB_HOST', 'dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-	define ('DB_NAME', 'xs74953pb0hvmidl');
-	define ('DB_USER', 'y0k79cxzb1n7lj9r');
-	define ('DB_PASSWORD', 'gk6clc7wdkfdevfw');
-	define ('DB_CHARSET', 'utf8');
+	//define ('DB_HOST', 'dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+	//define ('DB_NAME', 'xs74953pb0hvmidl');
+	//define ('DB_USER', 'y0k79cxzb1n7lj9r');
+	//define ('DB_PASSWORD', 'gk6clc7wdkfdevfw');
+	//define ('DB_CHARSET', 'utf8');
 	//define("BASE_URL", "http://localhost/tienda_virtual/")
 
-	/*Datos de conexión a Base de Datos
-	define ('DB_HOST', 'localhost');
+	//Datos de conexión a Base de Datos
+	//define ('DB_HOST', 'localhost');
   
-	/*Datos de conexión a Base de Datos
+	//Datos de conexión a Base de Datos
 	define ('DB_HOST', 'localhost');
 	define ('DB_NAME', 'db_tiendavirtual');
 	define ('DB_USER', 'root');
 	define ('DB_PASSWORD', '');
 	define ('DB_CHARSET', 'utf8');
-	//define("BASE_URL", "http://localhost/tienda_virtual/")*/
+	//define("BASE_URL", "http://localhost/tienda_virtual/")
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
 	define ('SPD',".");
