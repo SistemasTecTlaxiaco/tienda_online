3713cbd4d1faacae43e58bf09ffd3131b28cc724
JulioCesar69 <juliocrack912@gmail.com>
2022-04-19 17:48:47 -0500

    Configuración vistas RA

--- Views/Raumentada/raumentada.php
+++ Views/Raumentada/raumentada.php
@@ -5,18 +5,18 @@
 <div class="menu-desktop">
 	<ul class="main-menu">
 		<li class="active-menu">
-        <img src="<?=media().'/images/teclado.jpg'?>"  height="400px" width="400px"/>
-			<a href="<?= base_url(); ?>">Teclado</a>
+        <a href="<?= base_url().'/Views/Raumentada/teclado.php' ?>"><<img src="<?=media().'/images/teclado.jpg'?>"  height="400px" width="400px"/></a>
+			<a href="<?= base_url().'/Views/Raumentada/laptop.php' ?>">Teclado</a>
 		</li>
 
 		<li>
-        <img src="<?=media().'/images/laptop.jpg'?>" height="400px" width="400px"/>
-		    <a href="<?= base_url(); ?>/tienda">Laptop</a>
+        <a href="<?= base_url().'/Views/Raumentada/laptop.php' ?>"><img src="<?=media().'/images/laptop.jpg'?>" height="400px" width="400px"/></a>
+		    <a href="<?= base_url().'/Views/Raumentada/laptop.php' ?>">Laptop</a>
 		</li>
 
 		<li>
-        <img src="<?=media().'/images/bocinas.jpg'?>" height="400px" width="400px"/>
-			<a href="<?= base_url(); ?>/carrito">Bocinas</a>
+        <a href="<?= base_url().'/Views/Raumentada/bocina.php' ?>"><img src="<?=media().'/images/bocinas.jpg'?>" height="400px" width="400px"/></a>
+		    <a href="<?= base_url().'/Views/Raumentada/bocina.php' ?>">Bocinas</a>
 		</li>
 	</ul>
 </div>
